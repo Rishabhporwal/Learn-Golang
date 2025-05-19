7da752ca1ecf8e42965e29dcb315b4061a364cd7
Rishabh Porwal <rishabhporwal95@gmail.com>
2025-05-20 01:47:20 +0530

    if statement

---
+++ 6_if_else/ifelse.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	// age := 15
+
+	// if age >= 18 {
+	// 	print("Adult")
+	// } else if age >= 12 {
+	// 	println("Teenager")
+	// } else {
+	// 	print("Kid")
+	// }
+
+	if age := 30; age < 18 {
+		println("Teenager")
+	} else {
+		println("Adult")
+	}
+
+}
